63e9fe8d1e1477567a1b79a8b973c7a2dec55fa3
Gabriel Henrique <ev04hs@gmail.com>
2025-02-21 10:30:48 -0300

    product entity creation

---
+++ sales-api/src/main/java/io/github/gabrielhs33/salesapi/model/Product.java
@@ -0,0 +1,74 @@
+package io.github.gabrielhs33.salesapi.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+    @Column
+    private String description;
+    @Column
+    private String sku;
+    @Column(name="price", precision = 2, scale = 16)
+    private BigDecimal price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", sku='" + sku + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ sales-api/src/main/java/io/github/gabrielhs33/salesapi/model/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package io.github.gabrielhs33.salesapi.model.repository;
+
+
+import io.github.gabrielhs33.salesapi.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    
+}
